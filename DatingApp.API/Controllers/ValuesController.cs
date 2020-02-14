0b0a9ffa472b9647efbeea24457c929e44bb9f45
jamesliuli <jamesliuli@gmail.com>
2020-02-14 09:20:24 -0500

    review security

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-    // [Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -21,7 +21,6 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values
-        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -42,6 +42,8 @@ namespace DatingApp.API
             
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
+
+            //use jwt token
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters{
