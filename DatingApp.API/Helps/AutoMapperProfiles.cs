1d945623c48f9ddaae4ef182cfc019ce47aa8e19
jamesliuli <jamesliuli@gmail.com>
2020-02-17 12:05:12 -0500

    Add AutoMap

--- DatingApp.API/Helps/AutoMapperProfiles.cs
+++ DatingApp.API/Helps/AutoMapperProfiles.cs
@@ -11,10 +11,16 @@ namespace DatingApp.API.Helps
         {
             CreateMap<User, UserForListDto>()
             .ForMember( dest => dest.PhotoUrl, opt =>
-                   opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url));
+                   opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url))
+            .ForMember( dest => dest.Age, opt =>
+                   opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
             CreateMap<User, UserDetailedDto>()
             .ForMember( dest => dest.PhotoUrl, opt =>
-                   opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url));
+                   opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url))
+            .ForMember( dest => dest.Age, opt =>
+                   opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+                   
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
