a1a4cec66fd21055d3f941b1c6f2377d7cfe5781
jamesliuli <jamesliuli@gmail.com>
2020-02-22 00:33:17 -0500

    add message entity

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -29,6 +29,9 @@ namespace DatingApp.API.Models
         public ICollection<Like> Likers {get; set;}
         public ICollection<Like> Likees {get; set;}
 
+        public virtual ICollection<Message> MessagesSent { get; set; }
+        public virtual ICollection<Message> MessagesReceived { get; set; }
+
     }
 
 }
